3eb176d7376f5f1ce1ac58238faee37b177e74c4
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-28 22:03:14 +0000

    Card3 and Stat test ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Badge from "./components/ui/Badge"; // importe ton Badge (ajuste le chemi
 import Card1 from "./components/ui/Card1"; // importe ton Card1 (ajuste le chemin si besoin)
 import Card2 from "./components/ui/Card2"; // importe ton Card2 (ajuste le chemin si besoin)
 import CustomButton from "./components/ui/CustomButton"; // importe ton Button (ajuste le chemin si besoin)
+import Card3 from "./components/ui/Card3"; // importe ton Card3 (ajuste le chemin si besoin)
+import Stat from "./components/ui/Stat"; // importe ton Stat (ajuste le chemin si besoin)
 import Arrow from "./assets/icons/Arrow.svg";
 
 import Shirt from "./assets/images/chemise.jpeg";
@@ -21,33 +23,33 @@ function App() {
 
         <Badge label="Sustainable" label2="Materials" />
 
-       <Badge label="Timeless Design"/>
+        <Badge label="Timeless Design" />
 
-        <Badge label="Personal Touch"/>
+        <Badge label="Personal Touch" />
       </div>
 
       {/* Card1 */}
 
       <div className=" w-full h-10vh flex gap-10">
         <Card1
-        category="Habits"
-        title="Chemise"
-        description="Chemise élégante en crochet, confectionnée en laine douce et respirante.Son style artisanal apporte une touche bohème et chaleureuse, idéale pour un look à la fois chic et décontracté."
-        price={"50"}
-        imageUrl={Shirt}
-        rating={5}
-        onFavoriteClick={() => handleFavorite("Chemise")}
-      />
+          category="Habits"
+          title="Chemise"
+          description="Chemise élégante en crochet, confectionnée en laine douce et respirante.Son style artisanal apporte une touche bohème et chaleureuse, idéale pour un look à la fois chic et décontracté."
+          price={"50"}
+          imageUrl={Shirt}
+          rating={5}
+          onFavoriteClick={() => handleFavorite("Chemise")}
+        />
 
-      <Card1
-        category="Habits"
-        title="Top"
-        description="Top en crochet réalisé à la main avec une laine douce et légère. Son design unique apporte une touche bohème et cosy, parfait à porter seul en été ou superposé en saison fraîche."
-        price={"10"}
-        imageUrl={Top}
-        rating={5}
-        onFavoriteClick={() => handleFavorite("Top")}
-      />
+        <Card1
+          category="Habits"
+          title="Top"
+          description="Top en crochet réalisé à la main avec une laine douce et légère. Son design unique apporte une touche bohème et cosy, parfait à porter seul en été ou superposé en saison fraîche."
+          price={"10"}
+          imageUrl={Top}
+          rating={5}
+          onFavoriteClick={() => handleFavorite("Top")}
+        />
       </div>
 
       {/* Card2 */}
@@ -64,22 +66,53 @@ function App() {
       {/* Button */}
 
       <div className="mt-7 flex gap-5 w-full" >
-        <CustomButton 
-        text = "Click me"
-        background = "bg-brownDark"
-        textColor = "text-white"
-         ></CustomButton>
+        <CustomButton
+          text="Click me"
+          background="bg-brownDark"
+          textColor="text-white"
+        ></CustomButton>
 
-        <CustomButton 
-        text = "Click me again"
-        background = "bg-ivory"
-        textColor = "text-brownDark"
-        border = {true}
-        borderColor = "border-brownDark"
-        icon={<img src={Arrow} alt="arrow icon" className="w-6 h-6" />}
-         ></CustomButton>
+        <CustomButton
+          text="Click me again"
+          background="bg-ivory"
+          textColor="text-brownDark"
+          border={true}
+          borderColor="border-brownDark"
+          icon={<img src={Arrow} alt="arrow icon" className="w-6 h-6" />}
+        ></CustomButton>
 
 
+      </div>
+
+      {/* Card3 */}
+      <div className="mt-7 flex gap-5 w-full">
+        <Card3
+          backgroundImg={Shirt}
+          chipText="1"
+          chipBgColor="bg-brownDark"
+          chipTextColor="text-ivory"
+          mainTitle="Chemise"
+          description="Chemise élégante en crochet, confectionnée en laine douce et respirante.Son style artisanal apporte une touche bohème et chaleureuse, idéale pour un look à la fois chic et décontracté."
+        ></Card3>
+
+        <Card3
+          backgroundImg={Top}
+          chipText="2"
+          chipBgColor="bg-brownDark"
+          chipTextColor="text-ivory"
+          mainTitle="TEST"
+          description="Chemise élégante en crochet, confectionnée en laine douce et respirante.Son style artisanal apporte une touche bohème et chaleureuse, idéale pour un look à la fois chic et décontracté."
+        ></Card3>
+      </div>
+
+      {/* Stat */}
+      <div className="mt-7 flex gap-5 w-full" >
+        <Stat
+          mainTitle="150+"
+          mainTitleColor="text-brownDark"
+          description="Happy Customers"
+          descriptionColor="text-brownLight"
+        />
       </div>
 
     </div>
