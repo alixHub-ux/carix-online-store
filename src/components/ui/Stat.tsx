54377dca7d046ad1a876bb51faa54bafa9fb0936
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-28 22:04:59 +0000

    Stat reusable component completly build

---
+++ src/components/ui/Stat.tsx
@@ -0,0 +1,34 @@
+type StatProps = {
+    mainTitle?: string;
+    mainTitleColor?: string;
+    descriptionColor?: string;
+    description?: string;
+};
+
+const Stat: React.FC<StatProps> = ({
+    mainTitleColor,
+    descriptionColor,
+    mainTitle,
+    description
+}) => {
+
+    const statStyle = `
+        ${descriptionColor ?? ""} 
+        ${mainTitleColor ?? ""} 
+        flex flex-col items-center justify-center text-center
+        w-[20vw] gap-3
+    `;
+
+    return (
+        <div className={statStyle}>
+            <div  className={`font-bold text-5xl`}>
+                {mainTitle}
+            </div>
+            <div  className={`text-2xl`}>
+                {description}
+            </div>
+        </div>
+    );
+};
+
+export default Stat;
